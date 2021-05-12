e11c3c11638621368c3c3e1380c5382ceabacf83
Logan Moss <lrdjmoss@gmail.com>
2021-05-11 23:00:10 -0400

    added to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,27 @@ import Layout from "../components/Layout";
 
 const About = () => (
     <Layout>
-        
+        <div class="about-layer-div">
+            <div id="about-box-div-1" class="about-box">
+            1
+            </div>
+            <div id="about-box-div-2" class="about-box">
+            2
+            </div>
+        </div>
+        <div class="about-layer-div">
+            <div id="about-box-div-3" class="about-box">
+            3
+            </div>
+            <div id="about-box-div-4" class="about-box">
+            4
+            </div>
+        </div> 
+        <div class="about-layer-div">
+            <div id="about-box-div-5" class="about-box">
+            5
+            </div>
+        </div> 
     </Layout>
 )
   
