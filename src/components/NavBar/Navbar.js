27a9fee1d5782dc3d9869f9ffce1d8b84a05a2af
Logan Moss <lrdjmoss@gmail.com>
2021-05-27 19:47:53 -0400

    added state to navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby";
 import { withPrefix } from "gatsby-link";
 import { FaAlignJustify } from "react-icons/fa";
@@ -6,16 +6,7 @@ import "./nav.css";
 import Logo from "./Logo";
 import isInBrowser from "./isInBrowser";
 
-if(isInBrowser) {
-  var pathCheck = window.location.pathname
-  var pathBool = false;
-  console.log(pathCheck)
-  if(pathCheck.includes("about")){
-    pathBool = true;
-  } else {
-    pathBool = false;
-  }
-}
+
 
 function openNav() {
 
@@ -24,38 +15,39 @@ function openNav() {
 
 // Helper called classNames
 // const isBrowser = typeof window !== "undefined";
-class Navbar extends React.Component {
+const Navbar = () => {
   
-  // const [url, setUrl] = useState("");
+  const [pathBool, setpathBool] = useState(false);
   // componentDidMount(){
   //   if (isBrowser) {
   //     // window = "";
   //   }
   // }
-  // // useEffect(()=> {
-  // //   console.log(props)
-  // // }, [url])
+  useEffect(()=> {
+    if(isInBrowser) {
+      var pathCheck = window.location.pathname;
+      setpathBool(pathCheck.includes("about"));
+    }
+  }, [])
   
-  render(){
-    return (
-      <div  className={pathBool ? "navbar-background-image" : ""}>
-        <div className={pathBool ? "navbar green-box" : "navbar"}>
-            <div className="nav-links">
-              <Link to="/" state={{urlTo: "/"}} className="nav-link" activeClassName="active-link">HOME</Link>
-              <Link to="/about" state={{urlTo: "about"}} className="nav-link" activeClassName="active-link">ABOUT</Link>
-              <Link to="/projects" state={{urlTo: "projects"}} className="nav-link" activeClassName="active-link">PROJECTS</Link>
-              <Link to="/" state={{urlTo: "/"}} id="title"><Logo></Logo></Link>
-              <Link to="/research" state={{urlTo: "research"}} className="nav-link" activeClassName="active-link">RESEARCH</Link>
-              <Link to="/news" state={{urlTo: "news"}} className="nav-link" activeClassName="active-link">NEWS</Link>
-              <Link to="/contact" state={{urlTo: "contact"}} className="nav-link" activeClassName="active-link">CONTACT</Link>
-            </div>
-            <div>
-              <button id="nav-btn-true" onClick={openNav}><FaAlignJustify id="nav-btn"/></button>
-            </div>
-        </div>
+  return (
+    <div className={pathBool ? "navbar-background-image" : ""}>
+      <div className={pathBool ? "navbar green-box" : "navbar"}>
+          <div className="nav-links">
+            <Link to="/" state={{urlTo: "/"}} className="nav-link" activeClassName="active-link">HOME</Link>
+            <Link to="/about" state={{urlTo: "about"}} className="nav-link" activeClassName="active-link">ABOUT</Link>
+            <Link to="/projects" state={{urlTo: "projects"}} className="nav-link" activeClassName="active-link">PROJECTS</Link>
+            <Link to="/" state={{urlTo: "/"}} id="title"><Logo></Logo></Link>
+            <Link to="/research" state={{urlTo: "research"}} className="nav-link" activeClassName="active-link">RESEARCH</Link>
+            <Link to="/news" state={{urlTo: "news"}} className="nav-link" activeClassName="active-link">NEWS</Link>
+            <Link to="/contact" state={{urlTo: "contact"}} className="nav-link" activeClassName="active-link">CONTACT</Link>
+          </div>
+          <div>
+            <button id="nav-btn-true" onClick={openNav}><FaAlignJustify id="nav-btn"/></button>
+          </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Navbar
