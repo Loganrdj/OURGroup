0a80bd934515293fef18401c1d111b15317f08c2
Logan Moss <lrdjmoss@gmail.com>
2021-06-02 23:31:49 -0400

    fixed query order

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -36,54 +36,54 @@ const Projects = (props) => (
             </Row>
             <Row className="projects-row">
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[0].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[0].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[0].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project1.childImageSharp.fluid} alt="This is a placeholder building image"/>
+                    <h6>BVLGARI Hotel (Former Chamber of Commerce)</h6>
+                    <p>OCT Group</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[1].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[1].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[1].owner}</p>
+                <Img className="grayscale-image" fluid={props.data.Project2.childImageSharp.fluid !== undefined ? props.data.Project2.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Wuhan LanLing District - Urban Planning</h6>
+                    <p>Wuhan Tourism Development Investment Group Co., Ltd.</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[2].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[2].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[2].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project3.childImageSharp.fluid !== undefined ? props.data.Project3.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Nanjing BaiZiTing - Heritage Precinct</h6>
+                    <p>Nanjing Zhong Shan Group / Shui On Group</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[3].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[3].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[3].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project4.childImageSharp.fluid !== undefined ? props.data.Project4.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Shandong - Outlet Mall</h6>
+                    <p>Shandong Wellness City Development</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[4].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[4].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[4].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project5.childImageSharp.fluid !== undefined ? props.data.Project5.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Suzhou Riverbank Redevelopment</h6>
+                    <p>Overseas Chinese Town Holdings Company (OCT Group)</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[5].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[5].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[5].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project6.childImageSharp.fluid !== undefined ? props.data.Project6.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>TaiLaiFang and TongYiLi - Urban Renewal</h6>
+                    <p>Shanghai Western Property Group</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[6].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[6].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[6].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project7.childImageSharp.fluid !== undefined ? props.data.Project7.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Rockbund</h6>
+                    <p>New Huangpu Group</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[7].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[7].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[7].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project8.childImageSharp.fluid !== undefined ? props.data.Project8.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Xian Textile City</h6>
+                    <p>Xian Changle City Development Co. Ltd.</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[8].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[8].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[8].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project9.childImageSharp.fluid !== undefined ? props.data.Project9.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Wuhan XianAn Estate - Restoration</h6>
+                    <p>Huafa Group</p>
                 </Col>
                 <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                    <Img className="grayscale-image" fluid={props.data.allFile.edges[9].node.childImageSharp.fluid} alt="This is a placeholder building image"/>
-                    <h6>{props.data.site.siteMetadata.portfolioData[9].name}</h6>
-                    <p>{props.data.site.siteMetadata.portfolioData[9].owner}</p>
+                    <Img className="grayscale-image" fluid={props.data.Project10.childImageSharp.fluid !== undefined ? props.data.Project10.childImageSharp.fluid : ""} alt="This is a placeholder building image"/>
+                    <h6>Zhang Yuan - Preservation Project</h6>
+                    <p>China Resources (Holdings) Co., Ltd.</p>
                 </Col>
             </Row>
         </Container>
@@ -94,24 +94,105 @@ export default Projects
 
 export const query = graphql`
 query projectsPage {
-    site {
-      siteMetadata {
-        portfolioData {
-          name
-          owner
+    Project1: file(
+        relativeDirectory: {glob: "Project-1-*"}, 
+        name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
-    allFile(filter: {relativeDirectory: {glob: "Project-*"}, name: {glob: "display*"}}) {
-        edges {
-          node {
-            childImageSharp {
-              fluid(maxWidth: 250, maxHeight: 250) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
+    Project2: file(
+      relativeDirectory: {glob: "Project-2-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
         }
+      }
+    }
+    Project3: file(
+      relativeDirectory: {glob: "Project-3-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project4: file(
+      relativeDirectory: {glob: "Project-4-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project5: file(
+      relativeDirectory: {glob: "Project-5-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project6: file(
+      relativeDirectory: {glob: "Project-6-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project7: file(
+      relativeDirectory: {glob: "Project-7-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project8: file(
+      relativeDirectory: {glob: "Project-8-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project9: file(
+      relativeDirectory: {glob: "Project-9-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    Project10: file(
+      relativeDirectory: {glob: "Project-10-*"}
+      name: {glob: "display*"}
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 250, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
     }
   }
-`
+`  
